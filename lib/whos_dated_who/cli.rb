4f62ca4a660b9d22413290a2835ac00d87fea106
Adrian Perez <adrianperez.deb@gmail.com>
2014-06-18 14:07:57 +0200

    Add example importer to RethinkDB

--- lib/whos_dated_who/cli.rb
+++ lib/whos_dated_who/cli.rb
@@ -11,8 +11,38 @@ module WhosDatedWho
     desc "query", "Return the relationship status of a celebrity"
     def query(name)
       result = Client.new.fetch(name)
-      puts "#{result[:status]}: #{result[:current_relationship][:human]}"
+      puts "#{result[:relationship_status]}: #{result[:current_relationship][:human]}"
       puts result[:current_relationship][:dates].join(", ")
+    end
+
+    desc "explore", "Return the relationship status of everyone included in celebs.yml"
+    def explore
+      results = []
+      celebs = my_celebs
+      celebs.each do |celeb|
+        puts "Processing #{celeb}..."
+        begin
+          results << Client.new.fetch(celeb)
+        rescue
+          puts "Error processing #{celeb}"
+        end
+      end
+      results
+    end
+
+    desc "import_mine", "Import my favorites into RethinkDB"
+    def import_mine
+      Importer.new('celebs', 'mine').fetch_and_import(my_celebs)
+    end
+
+    desc "import_maxim", "Import maxim hot 100"
+    def import_maxim
+      resp = Faraday.get('http://www.maxim.com/hot100/2014')
+    end
+
+    private
+    def my_celebs
+      YAML.load_file(File.expand_path('../../../celebs.yml', __FILE__))
     end
   end
 end
---
+++ lib/whos_dated_who/importer.rb
@@ -0,0 +1,31 @@
+module WhosDatedWho
+  class Importer
+    include RethinkDB::Shortcuts
+    attr_reader :conn, :logger, :table_name
+
+    def initialize(db, table_name)
+      @conn = r.connect(host: 'localhost', db: db || 'celebs')
+      @logger = Logger.new(STDOUT)
+      @table_name = table_name
+    end
+
+    def fetch_and_import(celebs)
+      celebs.each do |celeb|
+        begin
+          logger.info "Fetching #{celeb}"
+          result = Client.new.fetch(celeb)
+          logger.info "Importing #{celeb} to #{table_name}"
+          import(result)
+        rescue
+          first_name, last_name = celeb.split
+          r.table("missing_#{table_name}").insert({first_name: first_name, last_name: last_name}).run(conn)
+          logger.error("Error importing #{celeb}, added to missing")
+        end
+      end
+    end
+
+    def import(results)
+      r.table(table_name).insert(JSON.parse(results.to_json)).run(conn)
+    end
+  end
+end
